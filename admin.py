f593513935b8f128724e96fa79af4ec615eeb7a5
Volnetiks <volnetiks@gmail.com>
2024-06-10 17:05:42 +0200

    Added edit user feature

--- admin.py
+++ admin.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import current_user, login_required
 
 from .models.game import Game
@@ -14,19 +14,25 @@ def main():
         return redirect(url_for('main.profile'))
 
     db.execute("SELECT * FROM users")
-    usersData = db.fetchall()
+    users_data = db.fetchall()
+    db.execute("SELECT * FROM games")
+    games_data = db.fetchall()
     users = []
+    games = []
 
-    for user_data in usersData:
+    for user_data in users_data:
         user = User(user_data[0], user_data[1], user_data[2], user_data[3], [], user_data[4])
         db.execute("SELECT * FROM games WHERE user_id = %s;", (user.id,))
 
-        for gameData in db:
-            user.games.append(Game(gameData[0], gameData[1], gameData[2], gameData[3], gameData[4], gameData[5], gameData[6], gameData[7], gameData[8]))
+        for game_data in db:
+            user.games.append(Game(game_data[0], game_data[1], game_data[2], game_data[3], game_data[4], game_data[5], game_data[6], game_data[7], game_data[8]))
 
-        users.append(user)    
+        users.append(user)   
 
-    return render_template("main_admin.html", users=users, name=current_user.name)
+    for game_data in games_data:
+        games.append(Game(game_data[0], game_data[1], game_data[2], game_data[3], game_data[4], game_data[5], game_data[6], game_data[7], game_data[8]))
+
+    return render_template("main_admin.html", users=users, name=current_user.name, games=games)
 
 
 @login_required
@@ -50,5 +56,47 @@ def delete_user_confirm_post(uuid):
     
     db.execute("UPDATE games SET user_id = NULL WHERE user_id = %s;", (uuid,))
     db.execute("DELETE FROM users WHERE id = %s", (uuid,))
+
+    return redirect(url_for("admin.main"))
+
+@login_required
+@admin.route("/edit_game/<game_id>")
+def edit_game(game_id):
+    if not current_user.admin:
+        return redirect(url_for('main.profile'))
+    
+    db.execute("SELECT * FROM games WHERE id = %s;", (game_id,))
+    game_data = db.fetchone()
+
+    game = Game(game_data[0], game_data[1], game_data[2], game_data[3], game_data[4], game_data[5], game_data[6], game_data[7], game_data[8])
+    
+    return render_template("edit_game.html", game=game)
+
+@login_required
+@admin.route("/edit_game/<game_id>", methods=["POST"])
+def edit_game_post(game_id):
+    if not current_user.admin:
+        return redirect(url_for('main.profile'))
+        
+    if request.form.get("name"):
+        db.execute("UPDATE games SET name = %s WHERE id = %s;", (request.form.get("name"), game_id))
+
+    if request.form.get("category"):
+        db.execute("UPDATE games SET category = %s WHERE id = %s;", (request.form.get("category"), game_id))
+
+    if request.form.get("year"):
+        db.execute('UPDATE games SET year = %s WHERE id = %s;', (request.form.get("year"), game_id))
+
+    if request.form.get("minPlayer"):
+        db.execute('UPDATE games SET "minPlayer" = %s WHERE id = %s;', (request.form.get("minPlayer"), game_id))
+
+    if request.form.get("maxPlayer"):
+        db.execute('UPDATE games SET "maxPlayer" = %s WHERE id = %s;', (request.form.get("maxPlayer"), game_id))
+
+    if request.form.get("age"):
+        db.execute("UPDATE games SET age = %s WHERE id = %s;", (request.form.get("age"), game_id))
+
+    if request.form.get("length"):
+        db.execute("UPDATE games SET length = %s WHERE id = %s;", (request.form.get("length"), game_id))
 
     return redirect(url_for("admin.main"))
